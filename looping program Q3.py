bb50d81c2c1d6012b0dfce26a60ff023440d4562
R-RAJAMARI <130639027+R-RAJAMARI@users.noreply.github.com>
2023-06-26 18:58:14 +0530

    Update looping program Q3.py

--- looping program Q3.py
+++ looping program Q3.py
@@ -1,8 +1,10 @@
-a=int(input())
-b=int(input())
-for i in range(a+1,b):
-    for j in range(2,i):
-        if i%j==0:
-            break
-        print(i,end=",")
-
+a=int(input("enter starting range:"))
+b=int(input("enter ending range:"))
+for i in range(a,b+1):
+    f_c = 0
+    if i!=1:
+        for j in range(2,i):
+            if(i%j == 0):
+                f_c += 1
+        if (f_c==0):
+            print(i,end=' ')
